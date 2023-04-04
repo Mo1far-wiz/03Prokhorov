917c2a6776466d5ee8371c52382f78bc94f0d0f4
Oleksandr Prokhorov o.prokhorov@pinglestudio.com <o.prokhorov@pinglestudio.com>
2023-04-04 23:00:34 +0300

    Add project files.

---
+++ 03Prokhorov/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _03Prokhorov.Model;
+using _03Prokhorov.Model.CustomException;
+using _03Prokhorov.ViewModel;
+
+namespace _03Prokhorov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        private void ProceedButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ViewModel.ViewModel.CalculateAge(DatePicker.SelectedDate) || !Person.IsValidEmail(Email.Text))
+            {
+                return;
+            }
+            Person person = new Person(FirstName.Text, LastName.Text, Email.Text, DatePicker.SelectedDate.Value);
+            person.InitializeAsync();
+            FirstNameAns.Content = "Name : " + person.Name;
+            LastNameAns.Content = "Surname : " + person.Surname;
+            EmailAns.Content = "E-mail: " + person.EmailAddress;
+            BirthDayAns.Content = "D of birth: " + person.DateOfBirth.ToShortDateString();
+            IsAdult.Text = "Adult: " + person.IsAdult;
+            HoroscopeEnglish.Text = "Western: " + person.SunSign;
+            HoroscopeAsian.Text = "Chinese: " + person.ChineseSign;
+            IsBirthday.Text = "B-day: " + person.IsBirthday;
+
+
+        }
+
+        private void FirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        private void LastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+
+        private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FirstName.Text.Length > 0 && LastName.Text.Length > 0 && Email.Text.Length > 0 && DatePicker.SelectedDate != null)
+            {
+                ProceedButton.IsEnabled = true;
+            }
+            else
+            {
+                ProceedButton.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ 03Prokhorov/ViewModel/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using _03Prokhorov.Model.CustomException;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace _03Prokhorov.ViewModel
+{
+    internal class ViewModel
+    {
+        public static bool CalculateAge(DateTime? birthDate)
+        {
+            if (birthDate != null)
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - birthDate.Value.Year;
+                if (birthDate > today.AddYears(-age)) age--;
+                try
+                {
+                    if (age > 135)
+                    {
+                        throw new AgeTooLowException("AgeTooLowException");
+                    }
+                    else if (age < 0)
+                    {
+                        throw new AgeTooHighException("AgeTooHighException");
+                    }
+                    else
+                    {
+                        if (birthDate.Value.Month == today.Month && birthDate.Value.Day == today.Day)
+                            MessageBox.Show("Happy B-day!");
+                        return true;
+                    }
+                }
+                catch (AgeTooLowException atl)
+                {
+                    //MessageBox.Show("AgeTooLowException");
+                }
+                catch (AgeTooHighException ath)
+                {
+                    //MessageBox.Show("AgeTooHighException");
+                }
+            }
+            return false;
+        }
+    }
+}
